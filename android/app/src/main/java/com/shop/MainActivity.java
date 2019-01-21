e1bec6e60e499f5f74b6c0c546ee8e1f9905c6aa
Ruslan Petrukhin <Ruslan_Petrukhin@epam.com>
2019-01-21 11:23:14 +0300

    Renamed project

--- android/app/src/main/java/com/shop/MainActivity.java
+++ android/app/src/main/java/com/shop/MainActivity.java
@@ -1,4 +1,4 @@
-package com.layoutandstyleadvanced;
+package com.shop;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "LayoutAndStyleAdvanced";
+        return "Shop";
     }
 }
--- android/app/src/main/java/com/shop/MainApplication.java
+++ android/app/src/main/java/com/shop/MainApplication.java
@@ -1,4 +1,4 @@
-package com.layoutandstyleadvanced;
+package com.shop;
 
 import android.app.Application;
 
