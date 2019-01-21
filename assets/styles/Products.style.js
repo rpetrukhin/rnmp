d281b3129a4d0f3e02cdd611b35d39b2e1076163
Ruslan Petrukhin <Ruslan_Petrukhin@epam.com>
2019-01-21 16:58:51 +0300

    Completed homework Native APIs

--- assets/styles/Products.style.js
+++ assets/styles/Products.style.js
@@ -1,5 +1,7 @@
 import { StyleSheet } from 'react-native';
 
+import { coral } from './epamStyles';
+
 export default StyleSheet.create({
 	container: {
 		flex: 1,
@@ -10,5 +12,20 @@ export default StyleSheet.create({
 		textAlign: 'center',
 		paddingVertical: 20,
 		borderBottomWidth: 1,
+	},
+	modalContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'rgba(0, 0, 0, 0.6)',
+	},
+	modalContent: {
+		width: '80%',
+		backgroundColor: '#FFFFFF',
+		padding: 20,
+	},
+	errorMessage: {
+		color: coral,
+		fontSize: 30,
 	},
 });
