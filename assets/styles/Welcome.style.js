fd1b85f1c5c363dd790fd3bac4c5c50134a5d664
Ruslan Petrukhin <Ruslan_Petrukhin@epam.com>
2019-02-01 13:48:05 +0300

    Homework 3rd-party completed

---
+++ assets/styles/Welcome.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	welcomeText: {
+		fontSize: 40,
+		paddingHorizontal: 20,
+	},
+	animationContainer: {
+		width: '50%',
+		height: '50%',
+	},
+});
